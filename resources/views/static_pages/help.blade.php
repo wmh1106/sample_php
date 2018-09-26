6d3733582e2eed91d84db52c6c783790dcd5eda3
wmh1106 <406875886@qq.com>
2018-09-26 08:17:37 +0000

    完成简陋的静态页面

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title','help')
+
+@section('content')
+    <h1>help</h1>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// get 表明这个路由将会响应 GET 请求，并将请求映射到指定的控制器动作上。
+// 比方说，我们向 http://sample.test/ 发出了一个请求，
+// 则该请求将会由 StaticPagesController 的 home 方法进行处理
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
